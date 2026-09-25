24880fc29b46b44d32923963994626639fc71a1e
tsukuyomi09 <samuelesala.business@gmail.com>
2026-09-26 02:44:14 +0800

    chore: profile population engine with single raster worker

--- tools/population/engine.py
+++ tools/population/engine.py
@@ -21,7 +21,7 @@ from tile_index import (
 
 EXPECTED_RESOLUTION_DEGREES = 1 / 1200
 METHODS = ("fractional", "center")
-MAX_RASTER_WORKERS = 4
+MAX_RASTER_WORKERS = 1
 CENTER_MASK_LOCK = Lock()
 
 
